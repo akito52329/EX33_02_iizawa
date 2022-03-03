9ead1b888089794a6ce9e2f696e27bb3e13afdc8
sakuseimoto@gmail.com <sakuseimoto@gmail.com>
2022-03-03 09:20:05 +0900

    new

--- EX33_hint/Program.cs
+++ EX33_hint/Program.cs
@@ -6,8 +6,8 @@ namespace EX33_hint
     {
         static void Main(string[] args)
         {
-            RectAngle rectAngle1 = new RectAngle(10, 10);
-            RectAngle rectAngle2 = new RectAngle(10, 10);
+            RectAngle rectAngle1 = new RectAngle(3, 9);
+            RectAngle rectAngle2 = new RectAngle(13, 3);
             var rectAngle3 = rectAngle1 + rectAngle2;
             if (rectAngle1 == rectAngle2)
             {
--- EX33_hint/RectAngle.cs
+++ EX33_hint/RectAngle.cs
@@ -6,8 +6,8 @@ namespace EX33_hint
 {
     class RectAngle : ISurface, ICircumference, IBounds
     {
-        readonly public float width;
-        readonly public float height;
+        public float width = 0;
+        public float height = 0;
         public RectAngle(float width = 0, float height = 0)
         {
             this.width = width;
@@ -47,8 +47,32 @@ namespace EX33_hint
             var syntheticWidth = 0f;
             var syntheticHeight = 0f;
 
-            syntheticWidth = Math.Max(Math.Max(rectAngle1.width, rectAngle1.height), Math.Max(rectAngle2.width, rectAngle2.height));
-            syntheticHeight = Math.Min(rectAngle1.width, rectAngle1.height) + Math.Min(rectAngle2.width, rectAngle2.height);
+            if (rectAngle1.width == rectAngle2.width)//幅同士同じ
+            {
+                syntheticWidth = rectAngle2.width;
+                syntheticHeight = rectAngle1.height + rectAngle2.height;
+            }
+            else if(rectAngle1.width == rectAngle2.height)//rectAngle1の幅とrectAngle2の高さが同じ
+            {
+                syntheticWidth = rectAngle2.height;
+                syntheticHeight = rectAngle1.height + rectAngle2.width;
+
+            }
+            else if (rectAngle1.height == rectAngle2.width)//rectAngle1の高さとrectAngle2の幅が同じ
+            {
+                syntheticWidth = rectAngle1.width + rectAngle2.height;
+                syntheticHeight = rectAngle2.width;
+            }
+            else if (rectAngle1.height == rectAngle2.height)//高さ同士同じ
+            {
+                syntheticWidth = rectAngle1.width + rectAngle2.width;
+                syntheticHeight = rectAngle2.height;
+            }
+            else
+            {
+                syntheticWidth = Math.Max(Math.Max(rectAngle1.width, rectAngle1.height), Math.Max(rectAngle2.width, rectAngle2.height));
+                syntheticHeight = Math.Min(rectAngle1.width, rectAngle1.height) + Math.Min(rectAngle2.width, rectAngle2.height);
+            }
 
             return new RectAngle(syntheticHeight,syntheticWidth);
         }
